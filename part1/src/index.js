a850c5e855caefd76e5128bd97669de2b41640dd
AdrianFuFe <adrianfufe@gmail.com>
2022-07-04 18:40:52 +0200

    first commit

---
+++ part1/src/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name:'Half Stack application development',
+    parts: [
+      {
+        name:'Fundamentals of React',
+        exercises: 10,
+      },
+      {   
+        name:'Using props to pass data',
+        exercises: 7,
+      },
+      { 
+        name:'State of a component',
+        exercises: 14,
+      }
+    ]
+  }
+
+  return (
+  /*     <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div> 
+  */
+    <div>
+      <Header course={course} />
+      <Content parts={parts}/>
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
