0e94bafb6182708966a4a7619e48322506dd5ff1
AdrianFuFe <adrianfufe@gmail.com>
2022-07-15 12:30:24 +0200

    unicafe exercise finished

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,19 +4,37 @@ const Header = ({ title }) => <h2>{title}</h2>
 
 const Button = ({handleClick, text}) =>  <button onClick={handleClick}> {text} </button>
 
-const Statistics = (props) => {
-  if (props.total === 0) return <p>No feedback given</p>
+const Statistics = ({good, neutral, bad}) => {
+
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = good / total * 100;
+
+  if (total === 0) return <p>No feedback given</p>
 
   return (
     <>
-      <ul>
-        <li>good : {props.good}</li>
-        <li>neutral : {props.neutral}</li>
-        <li>bad : {props.bad}</li>
-        <li>total : {props.total}</li>
-        <li>average : {props.average}</li>
-        <li>positive : {props.positive}</li>
-      </ul>
+      <table>
+          <tbody>
+            <Statistic text="good : " value={good}/>
+            <Statistic text="neutral : " value={neutral}/>
+            <Statistic text="bad : " value={bad}/>
+            <Statistic text="total : " value={total}/>
+            <Statistic text="average : " value={average}/>
+            <Statistic text="positive : " value={positive + ' %'}/>
+          </tbody>
+      </table>
+    </>
+  )
+}
+
+const Statistic = ({text, value}) => {
+  return (
+    <>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
     </>
   )
 }
@@ -27,9 +45,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const total = good + neutral + bad;
-  const average = (good - bad) / total;
-  const positive = good / total * 100;
+
 
   return (
     <>
@@ -38,7 +54,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral +1)} text="neutral" />
       <Button handleClick={() => setBad(bad +1)} text="bad" />
       <Header title='statistics'/>
-      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </>
   )
 }
